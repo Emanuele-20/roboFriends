5c10acd90d06f66105cdcc149e7340fd06e9709a
Emanuele Pace <emanuele.10@outlook.it>
2021-04-05 13:51:40 +0100

    Create first Component named Hello and correctly exported in the index.js file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-import { render } from '@testing-library/react';
+//import { render } from '@testing-library/react';
 
 class App extends React.Component {
   render(){
---
+++ src/Hello.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+class Hello extends React.Component{
+
+    
+    render(){
+
+        return <h1>Hello London</h1>
+
+
+    }
+
+
+
+
+}
+
+export default Hello;
