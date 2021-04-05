e3b5477e458778c0593ac031d53e7e15f7bf64f6
Emanuele Pace <emanuele.10@outlook.it>
2021-04-05 14:19:38 +0100

    Added a prop to our component in the index.js file and specified that in the Hello component inside the Hello.js file

--- src/Hello.js
+++ src/Hello.js
@@ -8,9 +8,11 @@ class Hello extends React.Component{
     render(){
 
         return (
-            <div>
+            <div className='f2 tc'>
                 <h1>Hello London</h1>
                 <p>Here in London learning React</p>
+                    <p>{this.props.greeting}</p> 
+                {/* this is referring to the Hello Class */}
             </div>
         )
 
--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 //import App from './App';
 import Hello from './Hello'
+import 'tachyons'; 
 import reportWebVitals from './reportWebVitals';
 
 
 
 
 
-ReactDOM.render(  <Hello />,  document.getElementById('root')   );
+ReactDOM.render(  <Hello greeting = {'Hello React Ninja'}/>,  document.getElementById('root')   );
 
 
 
